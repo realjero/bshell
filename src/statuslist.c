d3572e01771d8e46124919d0a45d4703aefe7fa1
jerome <jerome_jero@t-online.de>
2023-05-09 17:56:14 +0200

    statuslist

--- src/statuslist.c
+++ src/statuslist.c
@@ -1,7 +1,48 @@
 #define _GNU_SOURCE
+
 #include <stdio.h>
 #include <stdlib.h>
 #include <unistd.h>
 #include <string.h>
+#include "list.h"
+#include "statuslist.h"
+
+List *status_list;
+
+void add_status(int pid, int pgid, char *status, char *program) {
+    Status *new_status = malloc(sizeof(Status));
+    new_status->pid = pid;
+    new_status->pgid = pgid;
+    new_status->status = status;
+    new_status->program = program;
+
+    List *list = status_list;
+    while (list != NULL) {
+        list = list->tail;
+    }
+    status_list = list_append(new_status, status_list);
+}
+
+void change_status(int pid, char *status) {
+    List *list = status_list;
+    while (list != NULL) {
+        Status *s = list->head;
+        if (s->pid == pid) {
+            s->status = status;
+            break;
+        }
+        list = list->tail;
+    }
+}
+
+void print_status_list() {
+    printf("PID\tPGID\tSTATUS\tPROGRAM\n");
+    List *list = status_list;
+    while (list != NULL) {
+        Status *s = list->head;
+        printf("%d\t%d\t%s\t%s\n", s->pid, s->pgid, s->status, s->program);
+        list = list->tail;
+    }
+}
 
 
--- src/statuslist.h
+++ src/statuslist.h
@@ -2,5 +2,15 @@
 
 #define STATUSLIST_H
 
+typedef struct {
+    int pid;
+    int pgid;
+    char *status;
+    char *program;
+} Status;
+
+void add_status(int pid, int pgid, char *status, char *program);
+void change_status(int pid, char *status);
+void print_status_list();
 
 #endif /* end of include guard: STATUSLIST_H */
